303fb735eb4f8942953dcc40ea96e6a7ce0d6f7a
Gilbert Kemell <kemegil009@gmail.com>
2024-06-13 14:09:48 -0300

    :sparkles: feat: reload data when sending series form

--- Vizer.Web/src/view/pages/series/useSeriesController.ts
+++ Vizer.Web/src/view/pages/series/useSeriesController.ts
@@ -11,13 +11,21 @@ interface IToggleSerieFormParams {
 export function useSeriesController() {
   const [series, setSeries] = useState<ISerieGetAllResponse[]>([])
   const [isLoading, setIsLoading] = useState(false)
+  const [isReload, setIsReload] = useState(false)
   const [isSerieFormOpen, setIsSerieFormOpen] = useState(false)
 
-  const toggleSerieForm = (params: IToggleSerieFormParams) =>
-    setIsSerieFormOpen(params.state === 'open')
+  function toggleSerieForm (params: IToggleSerieFormParams) {
+    if (params.state === 'open') {
+      setIsSerieFormOpen(true)
+      return
+    }
+    setIsSerieFormOpen(false)
+    setIsReload(!!params.isReloadData)
+  }
 
   async function getSeries() {
     try {
+      setIsReload(false)
       setIsLoading(true)
       setSeries(await serieService.getAll())
     } 
@@ -28,7 +36,7 @@ export function useSeriesController() {
 
   useEffect(() => {
     getSeries()
-  }, [])
+  }, [isReload])
 
   return {
     isLoading,

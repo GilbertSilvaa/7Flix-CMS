ef44971a51853f778a4094aa882c0381de2cdc40
gilbertsilvaa <kemegil515@gmail.com>
2024-08-13 19:21:30 -0300

    Revert ":truck: chore: rename project 😅"
    This reverts commit 93b91e5587777fa2e5f40f7fddb4e16db014b565.

--- 7Flix.CMS.API/Dtos/DashboardDtos/Responses/DashboardResponseDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Vizer.API.Dtos.DashboardDtos.Responses;
-
-sealed public record DashboardResponseDto (
-  int MoviesCount,
-  int SeriesCount,
-  int EpisodesCount,
-  int VodCount
-);
---
+++ Vizer.API/Dtos/DashboardDtos/Responses/DashboardResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Vizer.API.Dtos.DashboardDtos.Responses;
+
+sealed public record DashboardResponseDto (
+  int MoviesCount,
+  int SeriesCount,
+  int EpisodesCount,
+  int VodCount
+);

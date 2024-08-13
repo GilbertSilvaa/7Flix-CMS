93b91e5587777fa2e5f40f7fddb4e16db014b565
gilbertsilvaa <kemegil515@gmail.com>
2024-08-13 19:09:41 -0300

    :truck: chore: rename project 😅

---
+++ 7Flix.CMS.API/Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Vizer.API.Services;
+namespace Vizer.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+sealed public class DashboardController : ControllerBase
+{
+  private readonly DashboardService _service = new();
+
+  [HttpGet]
+  public async Task<IActionResult> Get()
+  {
+    try
+    {
+      return Ok(await _service.Get());
+    }
+    catch (Exception ex)
+    {
+      return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+    }
+  }
+}
